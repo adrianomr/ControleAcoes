6884faf7db6d38c8581cfd22c7af1be7f60a0eed
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2020-05-04 17:35:54 -0300

    Chamando metodos no start da aplicacao

--- src/main/java/br/com/adrianorodrigues/ControleAcoes/ControleAcoesApplication.java
+++ src/main/java/br/com/adrianorodrigues/ControleAcoes/ControleAcoesApplication.java
@@ -1,20 +1,34 @@
 package br.com.adrianorodrigues.ControleAcoes;
 
+import br.com.adrianorodrigues.ControleAcoes.processor.ProcessDownloadBovespaCotacoesHistoricas;
+import br.com.adrianorodrigues.ControleAcoes.processor.ProcessSalvaAcoesHistoricasSequencial;
+import br.com.adrianorodrigues.ControleAcoes.processor.ProcessSalvaBovespaCotacoesHistoricasSequencial;
+import br.com.adrianorodrigues.ControleAcoes.processor.ProcessUnzipBovespaCotacoesHistoricas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ControleAcoesApplication {
+	@Autowired
+	private ProcessSalvaAcoesHistoricasSequencial processSalvaAcoesHistoricasSequencial;
+	@Autowired
+	private ProcessSalvaBovespaCotacoesHistoricasSequencial processSalvaBovespaCotacoesHistoricasSequencial;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleAcoesApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner run() throws Exception {
-//		return args -> {
-//			CotacaoCliente.getCotacoes(2020);
-//		};
-//	}
+	@Bean
+	public CommandLineRunner run() throws Exception {
+		return args -> {
+			ProcessDownloadBovespaCotacoesHistoricas.execute();
+			ProcessUnzipBovespaCotacoesHistoricas.execute();
+			processSalvaAcoesHistoricasSequencial.execute();
+			processSalvaBovespaCotacoesHistoricasSequencial.execute();
+		};
+	}
 
 }
