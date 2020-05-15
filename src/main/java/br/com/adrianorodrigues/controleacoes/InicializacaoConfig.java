21f2c58a2f7ea81c42f6070a72cd4b77fe892ea3
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2020-05-15 19:00:57 -0300

    Ajustando para rodar somente se não houver dados no banco

--- src/main/java/br/com/adrianorodrigues/controleacoes/InicializacaoConfig.java
+++ src/main/java/br/com/adrianorodrigues/controleacoes/InicializacaoConfig.java
@@ -5,28 +5,27 @@ import br.com.adrianorodrigues.controleacoes.processor.ProcessDownloadBovespaCot
 import br.com.adrianorodrigues.controleacoes.processor.ProcessSalvaAcoesHistoricasSequencial;
 import br.com.adrianorodrigues.controleacoes.processor.ProcessSalvaBovespaCotacoesHistoricasSequencial;
 import br.com.adrianorodrigues.controleacoes.processor.ProcessUnzipBovespaCotacoesHistoricas;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.adrianorodrigues.controleacoes.repository.AcaoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-public class inicializacaoConfig {
-    @Autowired
-    private ProcessSalvaAcoesHistoricasSequencial processSalvaAcoesHistoricasSequencial;
-    @Autowired
-    private ProcessSalvaBovespaCotacoesHistoricasSequencial processSalvaBovespaCotacoesHistoricasSequencial;
-
+public class InicializacaoConfig {
     @Bean
     @Profile("!test")
-    public CommandLineRunner run() throws Exception {
+    public CommandLineRunner run(ProcessSalvaAcoesHistoricasSequencial processSalvaAcoesHistoricasSequencial,
+                                 ProcessSalvaBovespaCotacoesHistoricasSequencial processSalvaBovespaCotacoesHistoricasSequencial,
+                                 AcaoRepository acaoRepository) {
         return args -> {
-            ProcessDownloadBovespaCotacoesHistoricas.execute();
-            ProcessUnzipBovespaCotacoesHistoricas.execute();
-            processSalvaAcoesHistoricasSequencial.execute();
-            processSalvaBovespaCotacoesHistoricasSequencial.execute();
-            StatusInicializacaoSingleton.getSingleton().setInicializou();
+            if (acaoRepository.count() <= 0) {
+                ProcessDownloadBovespaCotacoesHistoricas.execute();
+                ProcessUnzipBovespaCotacoesHistoricas.execute();
+                processSalvaAcoesHistoricasSequencial.execute();
+                processSalvaBovespaCotacoesHistoricasSequencial.execute();
+                StatusInicializacaoSingleton.getSingleton().setInicializou();
+            }
         };
     }
 }
