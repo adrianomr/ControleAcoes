b07bd9d5f625f67d1446d4c3e2d2409f60d57d83
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2020-05-16 12:11:09 -0300

    Ajustando caminho dos arquivos

--- src/main/java/br/com/adrianorodrigues/controleacoes/processor/ProcessUnzipBovespaCotacoesHistoricas.java
+++ src/main/java/br/com/adrianorodrigues/controleacoes/processor/ProcessUnzipBovespaCotacoesHistoricas.java
@@ -1,5 +1,6 @@
 package br.com.adrianorodrigues.controleacoes.processor;
 
+import br.com.adrianorodrigues.controleacoes.util.FileUtil;
 import br.com.adrianorodrigues.controleacoes.util.ZipUtil;
 
 import java.io.IOException;
@@ -7,7 +8,7 @@ import java.io.IOException;
 public class ProcessUnzipBovespaCotacoesHistoricas {
     public static void execute() {
         try {
-            ZipUtil.Unzip("src/main/resources/cotacoes/zip/COTAHIST_DIA.ZIP", "src/main/resources/cotacoes/txt");
+            ZipUtil.Unzip(FileUtil.getFolderCreateIfNotExists("/cotacoes/zip").getPath() + "/COTAHIST_DIA.ZIP", FileUtil.getFolderCreateIfNotExists("/cotacoes/txt").getPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
