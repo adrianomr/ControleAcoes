1456f205c3f1768d7ff1accc7aff829484c55f4b
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2020-05-16 14:08:12 -0300

    Corrigindo bug do zip

--- src/main/java/br/com/adrianorodrigues/controleacoes/processor/ProcessUnzipBovespaCotacoesHistoricas.java
+++ src/main/java/br/com/adrianorodrigues/controleacoes/processor/ProcessUnzipBovespaCotacoesHistoricas.java
@@ -4,13 +4,34 @@ import br.com.adrianorodrigues.controleacoes.util.FileUtil;
 import br.com.adrianorodrigues.controleacoes.util.ZipUtil;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static br.com.adrianorodrigues.controleacoes.util.ConcurrencyUtils.stop;
+
 
 public class ProcessUnzipBovespaCotacoesHistoricas {
-    public static void execute() {
-        try {
-            ZipUtil.Unzip(FileUtil.getFolderCreateIfNotExists("/cotacoes/zip").getPath() + "/COTAHIST_DIA.ZIP", FileUtil.getFolderCreateIfNotExists("/cotacoes/txt").getPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static int execute() {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        AtomicInteger atomicInt = new AtomicInteger(0);
+        int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
+        IntStream.range(1986, anoAtual)
+                .forEach(i -> {
+                    Runnable task = () -> {
+                        try {
+                            ZipUtil.Unzip(FileUtil.getFolderCreateIfNotExists("/cotacoes/zip").getPath() + "/COTAHIST_A" + i + ".ZIP", FileUtil.getFolderCreateIfNotExists("/cotacoes/txt").getPath());
+                            atomicInt.addAndGet(1);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    };
+                    executor.submit(task);
+                });
+        stop(executor);
+        System.out.println(atomicInt.get());
+        return atomicInt.get();
     }
 }
