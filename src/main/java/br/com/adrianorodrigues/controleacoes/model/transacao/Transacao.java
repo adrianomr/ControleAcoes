1bbb228cf9053d302626599caeb4da0c35b205ab
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2020-05-16 20:46:06 -0300

    Criando entidade Transacao

--- src/main/java/br/com/adrianorodrigues/controleacoes/model/transacao/Transacao.java
+++ src/main/java/br/com/adrianorodrigues/controleacoes/model/transacao/Transacao.java
@@ -2,51 +2,24 @@ package br.com.adrianorodrigues.controleacoes.model.transacao;
 
 
 import br.com.adrianorodrigues.controleacoes.model.Acao;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "transacao")
 public class Transacao {
     private int id;
     private Acao acao;
     private TipoTransacao tipoTransacao;
     private BigDecimal valor;
-
-    public Transacao(int id, Acao acao, TipoTransacao tipoTransacao, BigDecimal valor) {
-        this.id = id;
-        this.acao = acao;
-        this.tipoTransacao = tipoTransacao;
-        this.valor = valor;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Acao getAcao() {
-        return acao;
-    }
-
-    public void setAcao(Acao acao) {
-        this.acao = acao;
-    }
-
-    public TipoTransacao getTipoTransacao() {
-        return tipoTransacao;
-    }
-
-    public void setTipoTransacao(TipoTransacao tipoTransacao) {
-        this.tipoTransacao = tipoTransacao;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
 }
