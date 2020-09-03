b8e2eedbfcd76e480b83f438fa35109648632d25
Adriano de Mello Rodrigues <adriano.rodrigues@celk.net>
2020-09-03 20:55:00 -0300

    Desenvolvendo endpoint para buscar cotacao atual

---
+++ src/main/java/br/com/adrianorodrigues/controleacoes/dto/CotacaoAtualDTO.java
@@ -0,0 +1,11 @@
+package br.com.adrianorodrigues.controleacoes.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CotacaoAtualDTO {
+    String papel;
+    Double cotacao;
+}
